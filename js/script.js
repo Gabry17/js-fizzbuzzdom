49dab94682a312ff5dcfda996606552db0ef8a40
Gabry17 <gbianchi528@gmail.com>
2022-04-11 17:04:43 +0200

    aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 //fare un ciclo che stampa i numeri da 1 a 100
 for(let i = 1; i <= 100; i++){
     
-    
-    
     let multipli;
 
     if (i % 3 === 0) {
@@ -19,6 +17,28 @@ for(let i = 1; i <= 100; i++){
 
     console.log(i, multipli);
     
+
+    //BONUS
     
+    let yellowgreen;
+    if (i % 3 === 0) {
+        yellowgreen = 'yellow';
+    }
+
+    if (i % 5 === 0) {
+        yellowgreen = 'green';
+    }
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        yellowgreen = 'azure';
+    }
+
+    if (i % 3 !== 0 && i % 5 !== 0) {
+        multipli = i;
+    }
+
+    let box = document.querySelector('.container');
+    box.innerHTML += `<div class='row ${yellowgreen}'>${i, multipli}</div>`;
+
 }
 
