95bbe7a6e9520cbeb149b1a91ad5db3b263a4129
Gabry17 <gbianchi528@gmail.com>
2022-04-12 10:25:49 +0200

    correzione con soluzione piu efficiente

--- js/script.js
+++ js/script.js
@@ -1,43 +1,33 @@
 //fare un ciclo che stampa i numeri da 1 a 100
+
+let box = document.querySelector('.container');
+
 for(let i = 1; i <= 100; i++){
     
-    let multipli;
+    let multipli = i;
 
-    if (i % 3 === 0) {
-        multipli = 'fizz';
+    if (i % 3 === 0 && i % 5 === 0 ) {
+        multipli = 'FizzBuzz';
+    } else if (i % 3 === 0) {
+        multipli = 'Fizz';
+    } else if (i % 5 === 0) {
+        multipli = 'Buzz';
     }
 
-    if (i % 5 === 0) {
-        multipli = 'buzz';
-    }
-
-    if (i % 3 === 0 && i % 5 === 0) {
-        multipli = 'fizzbuzz';
-    }
-
-    console.log(i, multipli);
+    console.log(multipli);
     
-
     //BONUS
-    
-    let yellowgreen;
-    if (i % 3 === 0) {
-        yellowgreen = 'yellow';
-    }
 
-    if (i % 5 === 0) {
+    let yellowgreen;
+
+    if (i % 3 === 0 && i % 5 === 0 ) {
+        yellowgreen = 'azure';
+    } else if (i % 3 === 0) {
+        yellowgreen = 'yellow';
+    } else if (i % 5 === 0) {
         yellowgreen = 'green';
     }
 
-    if (i % 3 === 0 && i % 5 === 0) {
-        yellowgreen = 'azure';
-    }
-
-    if (i % 3 !== 0 && i % 5 !== 0) {
-        multipli = i;
-    }
-
-    let box = document.querySelector('.container');
     box.innerHTML += `<div class='row ${yellowgreen}'>${i, multipli}</div>`;
 
 }
